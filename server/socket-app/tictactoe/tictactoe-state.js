0aa985baff562747e0dbe7ffe675166229313c77
gvendur88 <gvendur88@gmail.com>
2016-12-14 12:26:06 +0000

    Both tests from lecture passed

--- server/socket-app/tictactoe/tictactoe-state.js
+++ server/socket-app/tictactoe/tictactoe-state.js
@@ -3,17 +3,28 @@ var _ = require('lodash');
 module.exports = function (injected) {
 
     return function (history) {
+				
+				let gamefull = false;
 
         function processEvent(event) {
+						if(event.type === "GameJoined") {
+								gamefull = true;
+						}
+						console.debug("event", event);
         }
 
         function processEvents(history) {
             _.each(history, processEvent);
         }
 
+				function gameFull() {
+						return gamefull;
+				}
+
         processEvents(history);
 
         return {
+						gameFull: gameFull,
             processEvents: processEvents
         }
     };
